7d3fdfdbb5fc7d5d71ce41b1d51de58d39aa2af0
Felix <info@strifel.de>
2022-02-22 16:42:31 +0100

    :tada: initial commit

---
+++ src/main/java/de/rfeoi/pundur/PundurMod.java
@@ -0,0 +1,31 @@
+package de.rfeoi.pundur;
+
+import net.minecraftforge.eventbus.api.SubscribeEvent;
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
+import net.minecraftforge.fml.event.server.FMLServerStartingEvent;
+import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+
+@Mod("pundur")
+public class PundurMod
+{
+    // Directly reference a log4j logger.
+    private static final Logger LOGGER = LogManager.getLogger();
+
+    public PundurMod() {
+        FMLJavaModLoadingContext.get().getModEventBus().addListener(this::setup);
+    }
+
+    private void setup(final FMLCommonSetupEvent event)
+    {
+        LOGGER.info("WE ARE ALIVE! PUNDUR FOREVER!");
+    }
+
+    @SubscribeEvent
+    public void onServerStarting(FMLServerStartingEvent event) {
+        LOGGER.info("HELLO from server starting");
+    }
+}
